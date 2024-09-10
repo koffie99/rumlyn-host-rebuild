fa5350ef5d952f96158d9a4cdac5515d9bd4c040
Joshua <koffworld@gmail.com>
2024-09-10 13:27:16 +0000

    charts added to dashboard

--- app/components/DashCard.jsx
+++ app/components/DashCard.jsx
@@ -1,11 +1,10 @@
 import React from "react"
-import { BsHouses } from "react-icons/bs"
 
-const DashCard = ({ count, item }) => {
+const DashCard = ({ icon, count, item }) => {
   return (
     <div className="bg-white p-8 rounded-lg shadow flex items-center gap-5">
       <div className="bg-[#f6fffd] p-4 rounded-lg">
-        <BsHouses className="text-lg" />
+        {React.createElement(icon, { className: "text-lg" })}
       </div>
       <div className="flex flex-col">
         <h2 className="text-3xl font-bold">{count}</h2>
--- app/tabs/Dashboard.jsx
+++ app/tabs/Dashboard.jsx
@@ -1,15 +1,92 @@
 import React from "react"
-import WhyCard from "../components/WhyCard"
 import DashCard from "../components/DashCard"
+import { BsHouses } from "react-icons/bs"
+import { GoPeople } from "react-icons/go"
+import { CiMoneyCheck1 } from "react-icons/ci"
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts"
 
 const Dashboard = () => {
+  // sample data
+  const data = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ]
+
   return (
     <div className="p-12">
       <h2 className="text-2xl">Dashboard</h2>
       <div className="mt-5 grid grid-cols-3 gap-4">
-        <DashCard count={2} item="Listings" />
-        <DashCard count={5} item="Followers" />
-        <DashCard count={3000} item="Earnings" />
+        <DashCard count={2} item="Listings" icon={BsHouses} />
+        <DashCard count={5} item="Followers" icon={GoPeople} />
+        <DashCard count={3000} item="Earnings" icon={CiMoneyCheck1} />
+      </div>
+
+      {/* Engagement Trend Line Chart */}
+      <div className="bg-white shadow-lg rounded-lg p-6 mt-8">
+        <h2 className="text-xl font-semibold mb-4">Engagement Trend</h2>
+        <ResponsiveContainer width="100%" height={200}>
+          <LineChart data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Line
+              type="monotone"
+              dataKey="pv"
+              stroke="#8884d8"
+              strokeWidth={2}
+              activeDot={{ r: 8 }}
+            />
+          </LineChart>
+        </ResponsiveContainer>
       </div>
     </div>
   )
