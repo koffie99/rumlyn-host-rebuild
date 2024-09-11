9c6e694cc42c62c63f5b961a4a06e26e940974cc
Joshua <koffworld@gmail.com>
2024-09-11 15:45:07 +0000

    earnings populated

--- app/tabs/Earnings.jsx
+++ app/tabs/Earnings.jsx
@@ -1,11 +1,75 @@
-import React from "react"
+import { Table } from "antd"
+import React, { useEffect, useState } from "react"
 
 const Earnings = () => {
+  const [earnings, setEarnings] = useState(null)
+
+  // get creator id
+  let creatorId
+
+  if (typeof sessionStorage !== "undefined") {
+    creatorId = sessionStorage.getItem("creatorId")
+  }
+
+  // get earnings
+  const getEarnings = async () => {
+    try {
+      const myHeaders = new Headers()
+      myHeaders.append(
+        "Authorization",
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjRkZWJiZmQ0NDBiOTFmYTBjOWRiYWUiLCJpYXQiOjE3MTYzODM0OTksImV4cCI6MTcxNjM4NzA5OX0.ilNNMAvFvzHRFEgUyWPNviQRLmpKfjs-Tqz_0R00Rbg"
+      )
+
+      const requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
+      }
+
+      fetch(
+        `https://api.rumlyn.com/api/v1/revenues/creator/${creatorId}`,
+        requestOptions
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          setEarnings(result.creator_revenue)
+        })
+        .catch((error) => console.error(error))
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    getEarnings()
+  }, [])
+
+  // columns
+  const columns = [
+    {
+      title: "Property",
+      dataIndex: "listing_name",
+      key: "listing_name",
+    },
+    {
+      title: "Price",
+      dataIndex: "amount",
+      key: "amount",
+    },
+    {
+      title: "Actions",
+      render: (_, record) => <div>Delete</div>,
+    },
+  ]
+
   return (
-    <div className="p-12">
+    <div className="md:p-12 p-5">
       <div className="flex items-center justify-between">
         <h2 className="text-2xl">Earnings</h2>
         <div></div>
+      </div>
+      <div className="mt-7 bg-white rounded-lg p-5 shadow">
+        <Table dataSource={earnings} columns={columns} />
       </div>
     </div>
   )
